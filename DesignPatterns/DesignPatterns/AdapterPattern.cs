c75aafa8ab1e0be5048cf624c568256022d4f5d8
Santosh Mali <santoshmalisp@gmail.com>
2022-03-25 17:26:24 +0530

    strategy pattern

---
+++ DesignPatterns/DesignPatterns/AdapterPattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// An adapter helps two incompatible interfaces to work together.
+    /// The Adapter design pattern allows otherwise incompatible classes to work together by converting the interface of one class into an interface expected by the clients.
+    /// </summary>
+    public class AdapterPattern
+    {
+    }
+
+
+}
---
+++ DesignPatterns/DesignPatterns/StrategyPattern.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class StrategyPattern
+    {
+    }
+
+    public interface ICloudStorageService
+    {
+        void SaveFile(FileStream fileStream);
+    }
+
+    public class AzureCloudStorageService : ICloudStorageService
+    {
+        public void SaveFile(FileStream fileStream)
+        {
+            //Use Azure Blob Storage
+        }
+    }
+
+    public class AwsCloudStorageService : ICloudStorageService
+    {
+        public void SaveFile(FileStream fileStream)
+        {
+            //Use Aws S3
+        }
+    }
+
+    public class GcpCloudStorageService : ICloudStorageService
+    {
+        public void SaveFile(FileStream fileStream)
+        {
+            //Use Google Cloud Storage
+        }
+    }
+
+    public class CloudStorage
+    {
+        private readonly ICloudStorageService _cloudStorageService;
+        public CloudStorage(ICloudStorageService cloudStorageService)
+        {
+            _cloudStorageService = cloudStorageService;
+        }
+
+        public void SaveFile(FileStream fileStream)
+        {
+            _cloudStorageService.SaveFile(fileStream);
+        }
+    }
+}
